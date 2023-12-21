e119710f7a84cedb9d776b2cda712e339abf0edd
Mayank <152572407+mayanksgit@users.noreply.github.com>
2023-12-21 07:07:24 +0000

    added code with comments

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 1777721  // cannot be changed (constant variable)
+let accountEmail = "mayank@gmail.com"
+var accountPassword = "x7ThalaForA_Reason"
+accountCity = "Bhopal" //JS supports this way of creating variables also (not recommended tho)
+
+// accountId = 234 // not allowed to change the value of a const
+
+accountEmail = "mayank2@gmail.com"
+accountPassword = "ThalaForever"
+accountCity = "New Delhi"  
+
+// accountEmail, accountPassword, accountCity can be modified
+
+
+// console.log(accountId)  // for this we have to use console.log() for every variable to get printed
+
+console.table([accountId, accountEmail, accountPassword, accountCity]) // printing all variables at the same time in a tabular format with index numbers
+
+/*
+Prefer not using var
+because of issues in block issues and functional issues
+*/
