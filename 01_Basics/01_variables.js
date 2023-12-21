20042b6bd297546183b205d4e3878a3b77f95206
Mayank <152572407+mayanksgit@users.noreply.github.com>
2023-12-21 13:15:14 +0000

    added some more info

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -3,6 +3,12 @@ let accountEmail = "mayank@gmail.com"
 var accountPassword = "x7ThalaForA_Reason"
 accountCity = "Bhopal" //JS supports this way of creating variables also (not recommended tho)
 
+// note: semi-colon is optional in JS
+
+// accountState;    // this will throw an error
+let accountState; // this variable is just declared and has no value. if you print this, it will print "undefined"
+
+
 // accountId = 234 // not allowed to change the value of a const (it will throw an error if you change the value of const)
 
 accountEmail = "mayank2@gmail.com"
@@ -14,7 +20,7 @@ accountCity = "New Delhi"
 
 // console.log(accountId)  // for this we have to use console.log() for every variable to get printed
 
-console.table([accountId, accountEmail, accountPassword, accountCity]) // printing all variables at the same time in a tabular format with index numbers
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) // printing all variables at the same time in a tabular format with index numbers
 
 /*
 Prefer not using var
