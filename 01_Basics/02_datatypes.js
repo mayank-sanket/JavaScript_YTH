285fc1b89601aff55e6f4e0f671304eca36189a9
Mayank <152572407+mayanksgit@users.noreply.github.com>
2023-12-22 06:54:57 +0000

    created file and added some code in it

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+// the earlier JS did not have the features of arrows, classes, modules, etc
+// but the latest JS has these features
+
+"use strict";   // treats all the JS code as the newer version
+// most of the times we don't need to write "use strict"; as the engine automatically treats the code as new version JS but....
+
+
+// alert("Your device is hacked!") // if you try to run this in TERMINAL (node), you will get an error. To run alert functions, BROWSER is recommended 
+// alert(3+3) // we are using nodejs, not browser
+
+// if you want to use alert function in NODE, there is a different syntax for it 
+
+// readability
+
+// console.log(3
+//     +3) 
+
+// (valid but not easy to read -- not recommended)
+
+// semicolon is optional in JS but you have to use it when there are two or more instructions in the same line
+// eg.
+
+// console.log(3+2); console.log("Mayank")  // here the semicolon was mandatory
